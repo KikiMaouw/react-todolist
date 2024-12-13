6484963ddfec694c0e764a18e5d3869b91ca9252
KikiMaouw <cristelle.hermand@gmail.com>
2024-12-13 12:12:41 +0100

    Inclusion LocalStorage + modification styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header.jsx';
 import TodoForm from './components/TodoForm.jsx';
 import TodoList from './components/TodoList.jsx';
-/*import './components/css/index.css'*/
+const LSKEY = "todos";
 
 
 const App = () => {
-    const [todos, setTodos] = useState([
-        { id: 1, text: 'Learn React', completed: false },
-        { id: 2, text: 'Be Awesome!', completed: false },
-    ]);
+    const [todos, setTodos] = useState(() => {
+        const savedTodos = localStorage.getItem(LSKEY);
+        return savedTodos ? JSON.parse(savedTodos) : [];
+    });
+
+    useEffect(() => {
+        localStorage.setItem(LSKEY, JSON.stringify(todos));
+    }, [todos]);
 
     const addTodo = (text) => {
         const newTodo = { id: Date.now(), text, completed: false };
@@ -29,7 +33,7 @@ const App = () => {
     };
 
     return (
-        <div>
+        <div className="app-container">
             <Header />
             <TodoForm addTodo={addTodo} />
             <h2>Todos</h2>
